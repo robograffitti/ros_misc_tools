557525cb63ceff3a5780783bf4200d0cb7089dc8
Yoshimaru Tanaka <y-tanaka@jsk.imi.i.u-tokyo.ac.jp>
2020-07-10 21:58:05 +0900

    Modified duration.

--- camera_misc_tools/scripts/rosbag_record_l515.sh
+++ camera_misc_tools/scripts/rosbag_record_l515.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rosbag record tf tf_static camera/color/image_rect_color camera/color/image_raw camera/color/camera_info camera/depth/image_rect_raw camera/depth/camera_info camera/aligned_depth_to_color/image_raw camera/aligned_depth_to_color/camera_info camera/aligned_depth_to_infra1/image_raw -O `rospack find camera_misc_tools`/bag/l515_`date "+%Y_%m%d_%H%M%S"`.bag --duration=1
+rosbag record tf tf_static camera/color/image_rect_color camera/color/image_raw camera/color/camera_info camera/depth/image_rect_raw camera/depth/camera_info camera/aligned_depth_to_color/image_raw camera/aligned_depth_to_color/camera_info camera/aligned_depth_to_infra1/image_raw -O `rospack find camera_misc_tools`/bag/l515_`date "+%Y_%m%d_%H%M%S"`.bag --duration=0.3
